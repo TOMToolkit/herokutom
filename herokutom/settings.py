182e23178bdb9e63544132803d7d71f889d6e47c
Austin Riba <austin@m51.io>
2019-02-19 16:02:23 -0800

    Use amazon s3 for serving files

--- herokutom/settings.py
+++ herokutom/settings.py
@@ -149,6 +149,15 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'data')
 MEDIA_URL = '/data/'
 
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', '')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRECT_ACCESS_KEY', '')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME', '')
+AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', '')
+AWS_DEFAULT_ACL = None
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -189,4 +198,4 @@ try:
 except ImportError:
     pass
 
-django_heroku.settings(locals())
+django_heroku.settings(locals(), staticfiles=False)
